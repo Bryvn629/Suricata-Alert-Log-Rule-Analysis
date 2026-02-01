f6f168b7d8a2e6def4a2204e9ac61cc52f17cd05
Bryvn629 <lawrencelabryan@icloud.com>
2026-02-01 16:23:51 -0500

    Add parse_eve.py to parse JSON alert events

---
+++ docs/scripts/parse_eve.py
@@ -0,0 +1,19 @@
+``python
+import json
+import sys
+
+def parse_eve(file_path):
+    with open(file_path, 'r') as f:
+        for line in f:
+            try:
+                data = json.loads(line)
+                if data.get("event_type") == "alert":
+                    print(json.dumps(data, indent=2))
+            except:
+                continue
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python parse_eve.py eve.json")
+    else:
+        parse_eve(sys.argv[1])
